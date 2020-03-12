dd98a729c4ecae1e5cc718ebc969a829aea1d829
Jared Bebb <j.bebb@live.com>
2020-03-12 16:20:09 -0500

    initial commit

---
+++ lib/View/GrowSensorView.cpp
@@ -0,0 +1,187 @@
+
+#include <Arduino.h>
+#include <Adafruit_TFTLCD.h> 
+#include <Adafruit_GFX.h>    
+#include <TouchScreen.h>
+#include <MCUFRIEND_kbv.h>
+#include "GrowSensorView.h"
+
+// The control pins for the LCD can be assigned to any digital or
+// analog pins...but we'll use the analog pins as this allows us to
+// double up the pins with the touch screen (see the TFT paint example).
+#define LCD_CS A3 // Chip Select goes to Analog 3
+#define LCD_CD A2 // Command/Data goes to Analog 2
+#define LCD_WR A1 // LCD Write goes to Analog 1
+#define LCD_RD A0 // LCD Read goes to Analog 0
+
+#define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
+
+// When using the BREAKOUT BOARD only, use these 8 data lines to the LCD:
+// For the Arduino Uno, Duemilanove, Diecimila, etc.:
+//   D0 connects to digital pin 8  (Notice these are
+//   D1 connects to digital pin 9   NOT in order!)
+//   D2 connects to digital pin 2
+//   D3 connects to digital pin 3
+//   D4 connects to digital pin 4
+//   D5 connects to digital pin 5
+//   D6 connects to digital pin 6
+//   D7 connects to digital pin 7
+// For the Arduino Mega, use digital pins 22 through 29
+// (on the 2-row header at the end of the board).
+
+// Assign human-readable names to some common 16-bit color values:
+#define	BLACK   0x0000
+#define	BLUE    0x001F
+#define	RED     0xF800
+#define	GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+// Color definitions
+#define ILI9341_BLACK       0x0000      /*   0,   0,   0 */
+#define ILI9341_NAVY        0x000F      /*   0,   0, 128 */
+#define ILI9341_DARKGREEN   0x03E0      /*   0, 128,   0 */
+#define ILI9341_DARKCYAN    0x03EF      /*   0, 128, 128 */
+#define ILI9341_MAROON      0x7800      /* 128,   0,   0 */
+#define ILI9341_PURPLE      0x780F      /* 128,   0, 128 */
+#define ILI9341_OLIVE       0x7BE0      /* 128, 128,   0 */
+#define ILI9341_LIGHTGREY   0xC618      /* 192, 192, 192 */
+#define ILI9341_DARKGREY    0x7BEF      /* 128, 128, 128 */
+#define ILI9341_BLUE        0x001F      /*   0,   0, 255 */
+#define ILI9341_GREEN       0x07E0      /*   0, 255,   0 */
+#define ILI9341_CYAN        0x07FF      /*   0, 255, 255 */
+#define ILI9341_RED         0xF800      /* 255,   0,   0 */
+#define ILI9341_MAGENTA     0xF81F      /* 255,   0, 255 */
+#define ILI9341_YELLOW      0xFFE0      /* 255, 255,   0 */
+#define ILI9341_WHITE       0xFFFF      /* 255, 255, 255 */
+#define ILI9341_ORANGE      0xFD20      /* 255, 165,   0 */
+#define ILI9341_GREENYELLOW 0xAFE5      /* 173, 255,  47 */
+#define ILI9341_PINK        0xF81F
+
+/******************* UI details */
+#define BUTTON_X 40
+#define BUTTON_Y 100
+#define BUTTON_W 60
+#define BUTTON_H 30
+#define BUTTON_SPACING_X 20
+#define BUTTON_SPACING_Y 20
+#define BUTTON_TEXTSIZE 2
+
+// text box where numbers go
+#define TEXT_X 10
+#define TEXT_Y 10
+#define TEXT_W 220
+#define TEXT_H 50
+#define TEXT_TSIZE 3
+#define TEXT_TCOLOR ILI9341_MAGENTA
+// the data (phone #) we store in the textfield
+#define TEXT_LEN 12
+char textfield[TEXT_LEN+1] = "";
+uint8_t textfield_i=0;
+
+#define YP A3  // must be an analog pin, use "An" notation!
+#define XM A2  // must be an analog pin, use "An" notation!
+#define YM 9   // can be a digital pin
+#define XP 8   // can be a digital pin
+
+//Touch For New ILI9341 TP
+#define TS_MINX 120
+#define TS_MAXX 900
+
+#define TS_MINY 70
+#define TS_MAXY 920
+// We have a status line for like, is FONA working
+#define STATUS_X 10
+#define STATUS_Y 65
+
+#define MINPRESSURE 10
+#define MAXPRESSURE 1000
+
+
+GrowSensorView::GrowSensorView(){
+  temperature = 0.0;
+  humidity = 0.0;
+}
+GrowSensorView::~GrowSensorView(){
+
+}
+
+void GrowSensorView::Setup(void) {
+
+  Serial.begin(9600);
+  Serial.println(F("TFT LCD test"));
+
+  #ifdef USE_Elegoo_SHIELD_PINOUT
+    Serial.println(F("Using Elegoo 2.8\" TFT Arduino Shield Pinout"));
+  #else
+    Serial.println(F("Using Elegoo 2.8\" TFT Breakout Board Pinout"));
+    Serial.println(F("Using Elegoo 2.8\" TFT Breakout Board Pinout"));
+  #endif
+
+  Serial.print("TFT size is "); Serial.print(tft.width()); Serial.print("x"); Serial.println(tft.height());
+
+  tft.reset();
+
+  uint16_t identifier = tft.readID();
+  if(identifier == 0x9325) {
+    Serial.println(F("Found ILI9325 LCD driver"));
+  } else if(identifier == 0x9328) {
+    Serial.println(F("Found ILI9328 LCD driver"));
+  } else if(identifier == 0x4535) {
+    Serial.println(F("Found LGDP4535 LCD driver"));
+  }else if(identifier == 0x7575) {
+    Serial.println(F("Found HX8347G LCD driver"));
+  } else if(identifier == 0x9341) {
+    Serial.println(F("Found ILI9341 LCD driver"));
+  } else if(identifier == 0x8357) {
+    Serial.println(F("Found HX8357D LCD driver"));
+  } else if(identifier==0x0101)
+  {     
+      identifier=0x9341;
+       Serial.println(F("Found 0x9341 LCD driver"));
+  }else {
+    Serial.print(F("Unknown LCD driver chip: "));
+    Serial.println(identifier, HEX);
+    Serial.println(F("If using the Elegoo 2.8\" TFT Arduino shield, the line:"));
+    Serial.println(F("  #define USE_Elegoo_SHIELD_PINOUT"));
+    Serial.println(F("should appear in the library header (Elegoo_TFT.h)."));
+    Serial.println(F("If using the breakout board, it should NOT be #defined!"));
+    Serial.println(F("Also if using the breakout, double-check that all wiring"));
+    Serial.println(F("matches the tutorial."));
+    identifier=0x9341;
+   
+  }
+
+  tft.begin(identifier);
+  tft.setRotation(2);
+  tft.fillScreen(BLACK);
+  
+  // default status message'
+  Status(F("Setup Screen"));
+}
+
+void GrowSensorView::Loop(){
+  tft.fillScreen(BLACK);
+  tft.setCursor(0, 0);
+  tft.setTextColor(GREEN);    tft.setTextSize(2);
+  tft.print("Humidity: ");
+  tft.print(humidity);
+  tft.println();
+  tft.print("Temp: ");
+  tft.print(temperature);
+  tft.print(" *F");
+  tft.println();
+}
+
+void GrowSensorView::Status(String msg) {
+  tft.fillRect(STATUS_X, STATUS_Y, 240, 8, BLACK);
+  tft.setCursor(STATUS_X, STATUS_Y);
+  tft.setTextColor(WHITE);
+  tft.setTextSize(1);
+  tft.print(msg);
+}
+
+void GrowSensorView::SetTemperature(float t){temperature=t;}
+void GrowSensorView::SetHumidity(float h){humidity=h;}
---
+++ lib/View/GrowSensorView.h
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+#include <Adafruit_TFTLCD.h> 
+#include <Adafruit_GFX.h>    
+#include <TouchScreen.h>
+#include <MCUFRIEND_kbv.h>
+using namespace std; 
+
+const int TEXT_LEN = 12;
+
+class GrowSensorView{
+    private:
+        String screen;
+        boolean buttonEnabled;
+
+    public:
+        GrowSensorView();
+        ~GrowSensorView();
+        void Setup();
+        void Loop();
+        void Status(String msg);
+        //MCUFRIEND_kbv tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+        MCUFRIEND_kbv tft;
+        // TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+        TSPoint p;
+        //TouchScreen ts;
+        float temperature;
+        float humidity;
+        void SetTemperature(float t);
+        void SetHumidity(float h);
+
+};
